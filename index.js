bf8c496a5356a08eb2c65949614c13c6af964c28
Cameron <cameronnakaba4@gmail.com>
2022-08-24 13:54:06 -0700

    event listener

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ input.addEventListener('click', function() {
   alert('I was clicked!');
 });
 }
+const input = document.getElementById('button');
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+});
